f82d5b7c4abd09645cb999a0845f80d12ac736e7
Andre Bauer <>
2023-08-10 10:05:55 -0500

    clean up

--- experiments/fake_experiments.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#python fake_injection.py --tweak_factor 4 --layer_number 8 --model gpt2-small --dataset 2wmh
-python fake_injection.py --tweak_factor 8 --layer_number 4 --model gpt2-large --dataset 2wmh
