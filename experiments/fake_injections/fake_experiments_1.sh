d4922e1aa6ffa588b3476b736efa79375988cead
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2023-09-11 00:44:20 +0000

    cleaned up results

--- experiments/fake_injections/fake_experiments_1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python fake_injection.py --tweak_factor 3 --layer_number 7 --model gpt2-small --dataset hand --fake_data_type nouns
-python fake_injection.py --tweak_factor 3 --layer_number 7 --model gpt2-small --dataset hand --fake_data_type verbs
-python fake_injection.py --tweak_factor 3 --layer_number 7 --model gpt2-small --dataset hand --fake_data_type adjectives
-python fake_injection.py --tweak_factor 3 --layer_number 7 --model gpt2-small --dataset hand --fake_data_type adverbs
-python fake_injection.py --tweak_factor 3 --layer_number 7 --model gpt2-small --dataset hand --fake_data_type conjunctions
-python fake_injection.py --tweak_factor 3 --layer_number 7 --model gpt2-small --dataset hand --fake_data_type top_5000
-
